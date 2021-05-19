e3e4f2496df2336e6ef51f5b84b821a1dbb2f715
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-05-19 18:17:48 -0300

    feat: heading component

---
+++ src/components/Heading/styles.ts
@@ -0,0 +1,43 @@
+import styled, { css, DefaultTheme } from 'styled-components';
+import media from 'styled-media-query';
+
+export type WrapperProps = {
+  color: 'white' | 'black';
+  lineLeft: boolean;
+  lineBottom: boolean;
+};
+
+const wrapperModifiers = {
+  lineLeft: (theme: DefaultTheme) => css`
+    padding-left: ${theme.spacings.xxsmall};
+    border-left: 0.7rem solid ${theme.colors.secondary};
+  `,
+
+  lineBottom: (theme: DefaultTheme) => css`
+    position: relative;
+    margin-bottom: ${theme.spacings.medium};
+
+    &::after {
+      position: absolute;
+      left: 0;
+      bottom: -1rem;
+      content: '';
+      width: 5rem;
+      border: 0.4rem solid ${theme.colors.primary};
+    }
+  `
+};
+
+export const Wrapper = styled.h2<WrapperProps>`
+  ${({ theme, color, lineLeft, lineBottom }) => css`
+    color: ${theme.colors[color!]};
+    font-size: ${theme.font.sizes.xlarge};
+
+    ${media.greaterThan('medium')`
+      font-size: ${theme.font.sizes.xxlarge};
+    `}
+
+    ${lineLeft && wrapperModifiers.lineLeft(theme)}
+    ${lineBottom && wrapperModifiers.lineBottom(theme)}
+  `}
+`;
