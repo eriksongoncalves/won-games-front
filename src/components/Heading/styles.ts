d5d23ec899e02a8707ee96d81f27fcdbce3fc3cd
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-05-20 18:02:00 -0300

    feat: add heading size and line colors

--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -1,35 +1,52 @@
 import styled, { css, DefaultTheme } from 'styled-components';
 import media from 'styled-media-query';
+import { LineColors, HeadingProps } from '.';
 
-export type WrapperProps = {
-  color: 'white' | 'black';
-  lineLeft: boolean;
-  lineBottom: boolean;
-};
+export type WrapperProps = Omit<HeadingProps, 'children'>;
 
 const wrapperModifiers = {
-  lineLeft: (theme: DefaultTheme) => css`
+  lineLeft: (theme: DefaultTheme, lineColor: LineColors) => css`
     padding-left: ${theme.spacings.xxsmall};
-    border-left: 0.7rem solid ${theme.colors.secondary};
+    border-left: 0.7rem solid ${theme.colors[lineColor]};
   `,
 
-  lineBottom: (theme: DefaultTheme) => css`
+  lineBottom: (theme: DefaultTheme, lineColor: LineColors) => css`
     position: relative;
     margin-bottom: ${theme.spacings.medium};
 
     &::after {
       position: absolute;
       left: 0;
-      bottom: -1rem;
+      bottom: -0.5rem;
       content: '';
       width: 5rem;
-      border: 0.4rem solid ${theme.colors.primary};
+      border-bottom: 0.5rem solid ${theme.colors[lineColor]};
     }
+  `,
+
+  small: (theme: DefaultTheme) => css`
+    font-size: ${theme.font.sizes.medium};
+
+    &::after {
+      width: 3rem;
+    }
+  `,
+
+  medium: (theme: DefaultTheme) => css`
+    font-size: ${theme.font.sizes.xlarge};
+
+    ${media.greaterThan('medium')`
+      font-size: ${theme.font.sizes.xxlarge};
+    `}
+  `,
+
+  huge: (theme: DefaultTheme) => css`
+    font-size: ${theme.font.sizes.huge};
   `
 };
 
 export const Wrapper = styled.h2<WrapperProps>`
-  ${({ theme, color, lineLeft, lineBottom }) => css`
+  ${({ theme, color, lineLeft, lineBottom, lineColor, size }) => css`
     color: ${theme.colors[color!]};
     font-size: ${theme.font.sizes.xlarge};
 
@@ -37,7 +54,8 @@ export const Wrapper = styled.h2<WrapperProps>`
       font-size: ${theme.font.sizes.xxlarge};
     `}
 
-    ${lineLeft && wrapperModifiers.lineLeft(theme)}
-    ${lineBottom && wrapperModifiers.lineBottom(theme)}
+    ${lineLeft && wrapperModifiers.lineLeft(theme, lineColor!)}
+    ${lineBottom && wrapperModifiers.lineBottom(theme, lineColor!)}
+    ${!!size && wrapperModifiers[size](theme)}
   `}
 `;
