01180b62588f4ab622932bffb8d645a604202aae
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-05-21 15:22:16 -0300

    chore: lint staged

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -9,14 +9,14 @@ import highlightMock from 'components/Highlight/mock';
 import Home from '.';
 
 const props = {
-  banners: bannerMock,
-  newGames: gamesMock,
+  banners: [bannerMock[0]],
+  newGames: [gamesMock[0]],
   mostPopularHighlight: highlightMock,
-  mostPopularGames: gamesMock,
-  upcommingGames: gamesMock,
+  mostPopularGames: [gamesMock[0]],
+  upcommingGames: [gamesMock[0]],
   upcommingHighligth: highlightMock,
-  upcommingMoreGames: gamesMock,
-  freeGames: gamesMock,
+  upcommingMoreGames: [gamesMock[0]],
+  freeGames: [gamesMock[0]],
   freeHighligth: highlightMock
 };
 
@@ -49,7 +49,7 @@ describe('<Home />', () => {
     // banner
     expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1);
     // card game
-    expect(screen.getAllByText(/population zero/i)).toHaveLength(20);
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(5);
     // highlight
     expect(screen.getAllByText(/read dead is back!/i)).toHaveLength(3);
   });
