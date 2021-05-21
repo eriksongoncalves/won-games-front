ccbd7dedc1cd98c20c478c53618df39afcea1887
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-05-21 11:07:25 -0300

    feat: remove main component and add home template

--- src/components/Main/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as S from './styles';
-
-export default function Main({ title = 'Hello World' }) {
-  return (
-    <S.Wrapper>
-      <S.Title>{title}</S.Title>
-    </S.Wrapper>
-  );
-}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,11 +9,11 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>Boilerplate - NextJS</title>
+        <title>Won Games</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="description" content="" />
+        <meta name="description" content="The best Game Stores in the world!" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
