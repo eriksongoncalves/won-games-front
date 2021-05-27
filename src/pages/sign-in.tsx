c000cce80dd253d8ce18b144991282ba43ccc365
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-05-27 00:25:10 -0300

    feat: sign in form component

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,7 +1,13 @@
 import Auth from 'templates/Auth';
 
+import { FormSignIn } from 'components';
+
 function SignIn() {
-  return <Auth title="Sign In">children</Auth>;
+  return (
+    <Auth title="Sign In">
+      <FormSignIn />
+    </Auth>
+  );
 }
 
 export default SignIn;
