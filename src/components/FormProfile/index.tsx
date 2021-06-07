3cded9baf6d52c17d04a399f7551087f80b44cef
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-06-07 11:54:55 -0300

    feat: form profile component

---
+++ src/components/FormProfile/index.tsx
@@ -0,0 +1,46 @@
+import { Button, Heading, TextField } from 'components';
+import Link from 'next/link';
+
+import * as S from './styles';
+
+export type FormProfileProps = {
+  username?: string;
+  email?: string;
+};
+
+const FormProfile = ({ email, username }: FormProfileProps) => (
+  <>
+    <Heading lineBottom color="black" size="small">
+      My profile
+    </Heading>
+
+    <S.Form>
+      <TextField
+        name="username"
+        placeholder="Username"
+        label="Username"
+        initialValue={username}
+      />
+
+      <TextField
+        name="email"
+        type="email"
+        placeholder="E-mail"
+        initialValue={email}
+        label="E-mail"
+        disabled
+      />
+
+      <S.ButtonContainer>
+        <Link href={`/forgot-password?email=${email}`} passHref>
+          <Button minimal size="medium" as="a">
+            Reset Password
+          </Button>
+        </Link>
+        <Button size="medium">Save</Button>
+      </S.ButtonContainer>
+    </S.Form>
+  </>
+);
+
+export default FormProfile;
