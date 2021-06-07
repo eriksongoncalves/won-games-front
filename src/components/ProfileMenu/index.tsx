fe984aa72e842fd0d4244ebc6cc257735e6b1646
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-06-07 11:40:26 -0300

    feat: profile menu component

---
+++ src/components/ProfileMenu/index.tsx
@@ -0,0 +1,47 @@
+import Link from 'next/link';
+// import { useRouter } from 'next/router';
+import {
+  AccountCircle,
+  ExitToApp,
+  FormatListBulleted
+} from '@styled-icons/material-outlined';
+
+import * as S from './styles';
+
+export type ProfileMenuProps = {
+  activeLink?: '/profile/me' | '/profile/cards' | '/profile/orders' | string;
+};
+
+const ProfileMenu = ({ activeLink }: ProfileMenuProps) => {
+  // const { push } = useRouter();
+
+  return (
+    <S.Nav>
+      <Link href="/profile/me" passHref>
+        <S.Link isActive={activeLink === '/profile/me'} title="My profile">
+          <AccountCircle size={24} />
+          <span>My profile</span>
+        </S.Link>
+      </Link>
+
+      <Link href="/profile/orders" passHref>
+        <S.Link isActive={activeLink === '/profile/orders'} title="My orders">
+          <FormatListBulleted size={24} />
+          <span>My orders</span>
+        </S.Link>
+      </Link>
+
+      <S.Link
+        role="button"
+        onClick={() => {
+          console.log('Sign Out');
+        }}
+      >
+        <ExitToApp size={24} title="Sign out" />
+        <span>Sign out</span>
+      </S.Link>
+    </S.Nav>
+  );
+};
+
+export default ProfileMenu;
