cb7440e5f5cf6f0e65b9f6aae88ef437344f684e
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-06-10 13:24:28 -0300

    feat: add dynamic banners on home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,35 @@
 import Home, { HomeTemplateProps } from 'templates/Home';
 
-import bannersMock from 'components/BannerSlider/mock';
 import gameMock from 'components/GameCardSlider/mock';
 import highlightMock from 'components/Highlight/mock';
+import { initializeApollo } from 'utils/apollo';
+import { QueryHome } from 'graphql/generated/QueryHome';
+import { QUERY_HOME } from 'graphql/queries/home';
 
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />;
 }
 
-export function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo();
+
+  const { data } = await apolloClient.query<QueryHome>({ query: QUERY_HOME });
+
   return {
     props: {
-      banners: bannersMock,
+      revalidate: 60,
+      banners: data.banners.map(banner => ({
+        img: `http://localhost:1337${banner.image?.url}`,
+        title: banner.title,
+        subtitle: banner.subtitle,
+        buttonLabel: banner.button?.label || null,
+        buttonLink: banner.button?.link || null,
+        ...(banner.ribbon && {
+          ribbon: banner.ribbon.text,
+          ribbonColor: banner.ribbon.color,
+          ribbonSize: banner.ribbon.size
+        })
+      })),
       newGames: gameMock,
       mostPopularHighlight: highlightMock,
       mostPopularGames: gameMock,
