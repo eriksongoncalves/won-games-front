4bf44048bd8b0917d288053674e5070751054085
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-06-18 17:52:27 -0300

    chore: use hook on cart dropdown

--- src/components/CartDropdown/index.tsx
+++ src/components/CartDropdown/index.tsx
@@ -1,19 +1,15 @@
 import { CartIcon, CartList, Dropdown } from 'components';
-import { GameItemProps } from 'components/GameItem';
 
 import * as S from './styles';
 
-export type CartDropdownProps = {
-  items?: GameItemProps[];
-  total?: string;
+const CartDropdown = () => {
+  return (
+    <S.Wrapper>
+      <Dropdown title={<CartIcon />}>
+        <CartList hasButton />
+      </Dropdown>
+    </S.Wrapper>
+  );
 };
-
-const CartDropdown = ({ items, total }: CartDropdownProps) => (
-  <S.Wrapper>
-    <Dropdown title={<CartIcon />}>
-      <CartList items={items} total={total} hasButton />
-    </Dropdown>
-  </S.Wrapper>
-);
 
 export default CartDropdown;
