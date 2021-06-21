40ccea2a3153442c1fcfa8bcb74d9899cb044076
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-06-21 18:57:02 -0300

    feat: integrate profile me

--- src/utils/apollo.ts
+++ src/utils/apollo.ts
@@ -1,21 +1,36 @@
-import { ApolloClient, HttpLink, NormalizedCacheObject } from '@apollo/client';
 import { useMemo } from 'react';
+import { Session } from 'next-auth';
+import { ApolloClient, HttpLink, NormalizedCacheObject } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import apolloCache from './apolloCache';
 
 let apolloClient: ApolloClient<NormalizedCacheObject | null>;
 
-function createApolloClient() {
+function createApolloClient(session?: Session | null) {
+  const httpLink = new HttpLink({
+    uri: `${process.env.NEXT_PUBLIC_API_URL}/graphql`
+  });
+
+  const authLink = setContext((_, { headers, session: clientSession }) => {
+    const jwt = session?.jwt || clientSession?.jwt || '';
+    const authorization = jwt ? `Bearer ${jwt}` : '';
+    return { headers: { ...headers, authorization } };
+  });
+
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
-    link: new HttpLink({ uri: 'http://localhost:1337/graphql' }),
+    link: authLink.concat(httpLink),
     cache: apolloCache
   });
 }
 
-export function initializeApollo(initialState = null) {
+export function initializeApollo(
+  initialState = null,
+  session?: Session | null
+) {
   // serve para verificar se já existe uma instância, para não criar outra
-  const apolloClientGlobal = apolloClient ?? createApolloClient();
+  const apolloClientGlobal = apolloClient ?? createApolloClient(session);
 
   // se a página usar o apolloClient no lado client
   // hidratamos o estado inicial aqui
@@ -31,7 +46,10 @@ export function initializeApollo(initialState = null) {
   return apolloClient;
 }
 
-export function useApollo(initialState = null) {
-  const store = useMemo(() => initializeApollo(initialState), [initialState]);
+export function useApollo(initialState = null, session?: Session) {
+  const store = useMemo(
+    () => initializeApollo(initialState, session),
+    [initialState, session]
+  );
   return store;
 }
