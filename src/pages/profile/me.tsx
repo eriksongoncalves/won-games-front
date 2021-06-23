4313c28765a6f22e3f2ea263f690dcba5c6b7b14
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-06-23 16:48:06 -0300

    fix: cache profile

--- src/pages/profile/me.tsx
+++ src/pages/profile/me.tsx
@@ -4,7 +4,10 @@ import FormProfile, { FormProfileProps } from 'components/FormProfile';
 import Profile from 'templates/Profile';
 import protectedRoutes from 'utils/protected-routes';
 import { initializeApollo } from 'utils/apollo';
-import { QueryProfileMe } from 'graphql/generated/QueryProfileMe';
+import {
+  QueryProfileMe,
+  QueryProfileMeVariables
+} from 'graphql/generated/QueryProfileMe';
 import { QUERY_PROFILE_ME } from 'graphql/queries/profile';
 
 export default function Me(props: FormProfileProps) {
@@ -19,11 +22,17 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await protectedRoutes(context);
   const apolloClient = initializeApollo(null, session);
 
-  const { data } = await apolloClient.query<QueryProfileMe>({
-    query: QUERY_PROFILE_ME
+  const { data } = await apolloClient.query<
+    QueryProfileMe,
+    QueryProfileMeVariables
+  >({
+    query: QUERY_PROFILE_ME,
+    variables: {
+      identifier: session?.id as string
+    }
   });
 
   return {
-    props: { session, username: data.me?.username, email: data.me?.email }
+    props: { session, username: data.user?.username, email: data.user?.email }
   };
 }
