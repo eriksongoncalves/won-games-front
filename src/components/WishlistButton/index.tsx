f423edfa695aced76d654ca9c9fb0fe394fb4601
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-06-23 15:34:34 -0300

    feat: wishlist button component

--- src/components/WishlistButton/index.tsx
+++ src/components/WishlistButton/index.tsx
@@ -1,18 +1,52 @@
-import { Favorite } from '@styled-icons/material-outlined';
+import { useWishlist } from 'hooks/use-wishlist';
+import { useSession } from 'next-auth/client';
 
 import Button, { ButtonProps } from 'components/Button';
 
+import { Favorite, FavoriteBorder } from '@styled-icons/material-outlined';
+
 type WishlistButtonProps = {
+  id: string;
   hasText?: boolean;
 } & Pick<ButtonProps, 'size'>;
 
-const WishlistButton = ({ hasText, size = 'small' }: WishlistButtonProps) => {
-  const ButtonText = 'Add to Wishlist';
+const WishlistButton = ({
+  id,
+  hasText,
+  size = 'small'
+}: WishlistButtonProps) => {
+  const [session] = useSession();
+
+  const {
+    isInWishlist,
+    addToWishlist,
+    removeFromWishlist,
+    loading: loadingApollo
+  } = useWishlist();
+
+  const handleClick = () => {
+    return isInWishlist(id) ? removeFromWishlist(id) : addToWishlist(id);
+  };
+
+  const ButtonText = isInWishlist(id)
+    ? 'Remove from Wishlist'
+    : 'Add to Wishlist';
+
+  if (!session) return null;
 
   return (
     <Button
-      icon={<Favorite aria-label={ButtonText} />}
+      icon={
+        isInWishlist(id) ? (
+          <Favorite aria-label={ButtonText} />
+        ) : (
+          <FavoriteBorder aria-label={ButtonText} />
+        )
+      }
+      onClick={handleClick}
+      minimal
       size={size}
+      disabled={loadingApollo}
       style={{ filter: 'none' }}
     >
       {hasText && ButtonText}
